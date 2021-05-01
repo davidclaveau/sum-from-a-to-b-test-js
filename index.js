2da423e461720e34bdf6ebe82c636401d78a2c68
David Claveau <daveclaveau@gmail.com>
2021-05-01 04:07:27 +0000

    Created recursive function that sums from x to y.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN < toN) {
+    fromN += sum((fromN + 1), toN)
+  }
+
+  return fromN;
 }
 
 module.exports = sum;
